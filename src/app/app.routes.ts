83a2b45bf4e508def29b9219c2d60118f032fa53
marco-soria <marco.soriaa7@gmail.com>
2024-06-03 19:45:47 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LabsComponent } from './pages/labs/labs.component';
+import { HomeComponent } from './pages/home/home.component';
+
+export const routes: Routes = [
+    { path: 'labs', component: LabsComponent },
+    { path: '', component: HomeComponent },
+    //{ path: '', redirectTo: '/home', pathMatch: 'full' }
+];
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-labs',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
 })
-export class AppComponent {
+export class LabsComponent {
   welcome = 'hola!';
   tasks = [
     'Install Angular CLI',
