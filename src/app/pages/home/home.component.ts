4528ea8817f2c0cacd1edc9bb9135e0a174d7e00
marco-soria <marco.soriaa7@gmail.com>
2024-06-04 10:26:31 -0500

    control structure

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { CommonModule} from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  tasks = signal([
+    'Install Angular CLI',
+    'Create project',
+    'Create component',
+    'Create  service',
+  ]);
 
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,14 +6,39 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrls: ['./labs.component.css']
 })
 export class LabsComponent {
-  welcome = 'hola!';
-  tasks = [
+  welcome = 'Welcome to my first app with angular';
+  tasks = signal([
     'Install Angular CLI',
     'Create project',
-    'Create components',
-    'Create service'
-  ]
+    'Create component',
+    'Create  service',
+  ]);
+  name = signal('Nicolas');
+  age = 18;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+
+  person = {
+    name: 'Nicolas',
+    age: 18,
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  }
+
+  clickHandler() {
+    alert('Hola')
+  }
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+
+  keydownHandler(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
